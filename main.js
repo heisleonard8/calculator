879bf8e06cae752231200e19f8e02bfd823c1cdb
unknown <leonardvin2006@gmail.com>
2025-06-17 06:08:16 -0700

    chore: Added some functionality to the Calculator

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 let display = document.getElementById("output");
-
-/**
- * When a user clicks on a button, it should display in the output
- */
-
 let buttons = document.querySelectorAll("button");
 
 for (let i = 0; i < buttons.length; i++) {
-  console.log(buttons[i].textContent);
+  let button = buttons[i];
+  let value = button.textContent;
+  button.addEventListener("click", () => {
+    if (value == "AC") {
+      display.textContent = "";
+    } else if (value == "DEL") {
+      display.textContent = display.textContent.slice(0, -1);
+    } else if (value == "=") {
+      display.textContent = eval(display.textContent);
+    } else {
+      display.textContent += value;
+    }
+  });
 }
